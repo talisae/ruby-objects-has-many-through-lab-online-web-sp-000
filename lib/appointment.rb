f7aeb9706124258d1f363732963fa618977c7930
Talisa Erazo <talisaerazo@gmail.com>
2019-01-22 02:39:34 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,26 @@
+class Appointment
+  attr_accessor :date, :doctor, :patient
+
+  @@all = []
+
+   def initialize(date= "", doctor= "", patient= "")
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def patients
+    self.patient
+  end
+
+  def doctor
+    self.doctor
+  end
+
+
+ end
